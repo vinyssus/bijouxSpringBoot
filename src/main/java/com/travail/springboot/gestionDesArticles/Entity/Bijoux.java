daac188790a7d7324d9fe1000413568f7f541c89
unknown <vinyssusrodolphe@gmail.com>
2022-09-27 01:59:24 +0200

    entity

--- src/main/java/com/travail/springboot/gestionDesArticles/Entity/Bijoux.java
+++ src/main/java/com/travail/springboot/gestionDesArticles/Entity/Bijoux.java
@@ -1,7 +1,24 @@
 package com.travail.springboot.gestionDesArticles.Entity;
 
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Bijoux {
 
+	private Integer id;
 	private String nom;
 	private double prix;
 	
